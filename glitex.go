ad6a7c8c78ad5225f29ba55d8c78ac130d4b56bf
Miłosz Pigłas <milosz@archeocs.com>
2016-06-21 20:14:02 +0200

    Standalone version

---
+++ glitex.go
@@ -0,0 +1,39 @@
+/*
+ *   Experimental extension for SQLite implemented in Go
+ *   Copyright (C) 2016  Milosz Piglas
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */ 
+
+package main
+
+import (
+    "C"
+    "strings"
+)
+
+//export PureJoin
+func PureJoin(sep string, values []string) string {
+    result :=  strings.Join(values, sep)
+    return result
+}
+
+//export CJoin
+func CJoin(sep string, values []string) (*C.char, int) {
+    result :=  strings.Join(values, sep)
+    return C.CString(result), len(result)
+}
+
+func main() {
+}
---
+++ run_glitex.c
@@ -0,0 +1,42 @@
+/*
+ *   Experimental extension for SQLite implemented in Go
+ *   Copyright (C) 2016  Milosz Piglas
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */ 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "glitexbase.h"
+
+int main() 
+{
+    GoString inputArray[3];
+    
+    inputArray[0].p = "xyz";
+    inputArray[0].n = 3;
+    
+    inputArray[1].p = "EEEE";
+    inputArray[1].n = 4;
+    
+    inputArray[2].p = "333333";
+    inputArray[2].n = 6;
+    
+    GoString sep = {";", 1};
+    GoSlice slice = { &inputArray[0], 3, 3 };
+    struct CJoin_return result = CJoin(sep, slice);
+    printf("%s\n", result.r0);
+    free(result.r0);
+    
+}
